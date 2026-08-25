0508c0997ae0802eb6e63c9590d84ce5522ea630
realking199 <braelenbediako@gmail.com>
2026-08-25 14:16:16 -0400

    Version 0.1.0

--- main.cpp
+++ main.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
+#include <cstdlib> // does the system stuff
+#include <thread>
+#include <chrono>
 
-int main(int, char**){
+int main()
+{
+
+    using namespace std::chrono_literals;
     std::cout << "Hello, from ChineseVirus!\n";
-    std::cin.get();
-    
+    std::this_thread::sleep_for(5s);
+    std::cout << "All your computer is mine.";
+    std::this_thread::sleep_for(3s);
+    std::system("dir /s");
+    std::cout << "Goodbye";
+    std::this_thread::sleep_for(1s);
+
 }
