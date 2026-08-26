fcabdc3afa3c4c82f4fab3c9168e1a342c40d932
Braelen Bediako <braelenbediako@gmail.com>
2026-08-26 13:00:14 -0400

    Release 1.0.0
    * Version 0.1.0
    
    * 1.0.0

--- main.cpp
+++ main.cpp
@@ -2,17 +2,18 @@
 #include <cstdlib> // does the system stuff
 #include <thread>
 #include <chrono>
+#include <direct.h>
+#include <filesystem>
 
 int main()
 {
 
+    _chdir("C:\\");
     using namespace std::chrono_literals;
     std::cout << "Hello, from ChineseVirus!\n";
     std::this_thread::sleep_for(5s);
     std::cout << "All your computer is mine.";
     std::this_thread::sleep_for(3s);
     std::system("dir /s");
-    std::cout << "Goodbye";
-    std::this_thread::sleep_for(1s);
 
 }
